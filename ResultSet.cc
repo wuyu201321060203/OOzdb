da6608662d1c17d6b2b6363d3a3a08e9e2bcca15
wuyu201321060203 <wuyuangelysys@gmail.com>
2014-08-25 16:43:53 +0800

    first post of ResultSet.h and ResultSet.cc

---
+++ ResultSet.cc
@@ -0,0 +1,112 @@
+#include "ResultSet.h"
+
+ResultSet::ResultSet(CONST_STDSTR name):_resultSetName(name)
+{
+    assert(_resultSetName);
+}
+
+ResultSet::~ResultSet()
+{
+}
+
+int ResultSet::getColumnCount()
+{
+}
+
+CONST_STDSTR ResultSet::getColumnName(int columnIndex)
+{
+}
+
+long ResultSet::getColumnSize(int columnIndex)
+{
+}
+
+int ResultSet::next()
+{
+}
+
+bool ResultSet::isnull(int columnIndex)
+{
+}
+
+CONST_STDSTR ResultSet::getString(int columnIndex)
+{
+}
+
+CONST_STDSTR ResultSet::getStringByName(CONST_STDSTR name)
+{
+    return getString(getIndex(name));
+}
+
+int ResultSet::getInt(int columnIndex)
+{
+}
+
+int ResultSet::getIntByName(CONST_STDSTR name)
+{
+    return getInt(getIndex(name));
+}
+
+long long ResultSet::getLLong(int columnIndex)
+{
+}
+
+long long ResultSet::getLLongByName(CONST_STDSTR name)
+{
+    return getLLong(getIndex(name));
+}
+
+double ResultSet::getDouble(int columnIndex)
+{
+}
+
+double ResultSet::getDoubleByName(CONST_STDSTR name)
+{
+    return getDouble(getIndex(name));
+}
+
+void const* ResultSet::getBlob(int columnIndex , int* size)
+{
+}
+
+void const* ResultSet::getBlobByName(CONST_STDSTR name , int* size)
+{
+    return getBlob(getIndex(name) , size);
+}
+
+time_t ResultSet::getTimestamp(int columnIndex)
+{
+}
+
+time_t ResultSet::getTimestampByName(CONST_STDSTR name)
+{
+    return getTimestamp(getIndex(name));
+}
+
+struct tm* ResultSet::getDateTime(int columnIndex, struct tm* tm)
+{
+}
+
+struct tm* ResultSet::getDateTimeByName(CONST_STDSTR name , struct tm* tm)
+{
+    return getDateTime(getIndex(name) , tm);
+}
+
+int ResultSet::checkAndSetColumnIndex(int columnIndex , int columnCount)
+{
+    int i = columnIndex - 1;
+    if (columnCount <= 0 || i < 0 || i >= columnCount)
+        throw( SQLException("Column index is out of range") );
+    return i;
+}
+
+int ResultSet::getIndex(CONST_STDSTR name)
+{
+    int i;
+    int columns = getColumnCount();
+    for (i = 1; i <= columns; i++)
+        if (name == getColumnName(i))
+            return i;
+    throw(SQLException(name ? name : "null"));
+    return -1;
+}
---
+++ ResultSet.h
@@ -0,0 +1,56 @@
+#ifndef RESULTSET_H
+#define RESULTSET_H
+
+#include <string>
+#include <time.h>//TODO
+#include <cassert>
+
+#include <boost/shared_ptr.hpp>
+
+#include <muduo/base/Logging.h>
+
+#include "Exception.h"
+#include "ResultSetDelegate.h"
+
+typedef std::string STDSTR ;
+#define CONST_STDSTR STDSTR const
+
+class ResultSet
+{
+public:
+
+    ResultSet(CONST_STDSTR name);
+    virtual ~ResultSet();
+    virtual int next();
+    virtual bool isnull(int columnIndex);
+    virtual CONST_STDSTR getString(int columnIndex);
+    CONST_STDSTR getStringByName(CONST_STDSTR name);
+    virtual int getInt(int columnIndex);
+    int getIntByName(CONST_STDSTR name);
+    virtual long long  getLLong(int columnIndex);
+    long long  getLLongByName(CONST_STDSTR name);
+    virtual double getDouble(int columnIndex);
+    double getDoubleByName(CONST_STDSTR name);
+    virtual void const* getBlob(int columnIndex , int* size);
+    void const* getBlobByName(CONST_STDSTR name , int* size);
+    virtual time_t getTimestamp(int columnIndex);//TODO
+    time_t getTimestampByName(CONST_STDSTR name);//TODO
+    virtual struct tm* getDateTime(int columnIndex, struct tm* tm);//TODO
+    struct tm* getDateTimeByName(CONST_STDSTR name, struct tm* tm);//TODO
+
+private:
+
+    CONST_STDSTR _resultSetName;
+
+private:
+
+    virtual int getColumnCount();
+    virtual CONST_STDSTR getColumnName(int columnIndex);
+    virtual long getColumnSize(int columnIndex);
+
+    static inline int checkAndSetColumnIndex(int columnIndex, int columnCount);
+};
+
+typedef boost::shared_ptr<ResultSet> ResultSetPtr;
+
+#endif
